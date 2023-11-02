8889ca05a55b2939de3380612e5a2504b65a8def
vlottetonny <57642354+vlottetonny@users.noreply.github.com>
2023-11-02 14:04:01 +0100

    Added tests and implemented a stop when tests fail for ci/cd fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import {NavBar} from "@/components/organisms/NavBar.organism";
-import {Footer} from "@/components/organisms/Footer.organism";
 import Head from "next/head";
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
